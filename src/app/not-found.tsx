7fd5ce816668e2db1429367115e20758032022b0
yacine4098 <noun.4199@gmail.com>
2025-11-14 18:57:15 +0100

    Force dynamic rendering for not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link'
 
+export const dynamic = 'force-dynamic'
+
 export default function NotFound() {
   return (
     <div className="min-h-screen flex items-center justify-center">
